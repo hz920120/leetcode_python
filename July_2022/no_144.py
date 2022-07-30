b46a9f927359fc8b0d598e4e52cedb2f784e934c
huangzuo <huangzuo@mi-ya.com.cn>
2022-07-30 16:46:57 +0800

    commit

--- July_2022/no_144.py
+++ July_2022/no_144.py
@@ -6,13 +6,45 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[int]
         """
-        res = []
-        def dfs(root):
-            if root is None:
-                return
-            res.append(root.val)
-            dfs(root.left)
-            dfs(root.right)
 
-        dfs(root)
+        # 简单递归
+        # res = []
+        # def dfs(root):
+        #     if root is None:
+        #         return
+        #     res.append(root.val)
+        #     dfs(root.left)
+        #     dfs(root.right)
+        #
+        # dfs(root)
+        # return res
+
+        # 栈解法
+        # if not root:
+        #     return []
+        # res, stack = [], [root]
+        # while stack:
+        #     curr = stack.pop()
+        #     if curr:
+        #         return
+        #     res.append(curr.val)
+        #     if curr.right:
+        #         stack.append(curr.right)
+        #     if curr.left:
+        #         stack.append(curr.left)
+        # return res
+
+        # 模板解法
+        if not root:
+            return []
+
+        curr, res, stack = root, [], []
+        while curr or stack:
+            while curr:
+                res.append(curr.val)
+                stack.append(curr)
+                curr = curr.left
+            tmp = stack.pop()
+            curr = tmp.right
+
         return res
